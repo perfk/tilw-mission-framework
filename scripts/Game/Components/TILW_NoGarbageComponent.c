51783f6506adc3063ed10175d16ada18c756e186
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2025-05-15 18:05:21 +0200

    Create TILW_NoGarbageComponent.c

---
+++ scripts/Game/Components/TILW_NoGarbageComponent.c
@@ -0,0 +1,8 @@
+[EntityEditorProps(category: "game/", description: "Prohibits collection by the garbage system.")]
+class TILW_NoGarbageComponentClass: ScriptComponentClass
+{
+}
+
+class TILW_NoGarbageComponent: ScriptComponent
+{
+}
